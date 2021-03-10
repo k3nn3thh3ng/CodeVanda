6fbfe982b68146d4cb36a1d4e478a1ae0e7f1c0e
root <kennethhengfucheng@gmail.com>
2021-03-10 08:25:41 +0000

    Serving react on express as static

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express        = require('express');
 const app            = express();
-const port           = process.env.PORT || 3001;
+const port           = process.env.PORT || 3000;
+const path           = require('path');
+
+app.use(express.static(path.join(__dirname, 'Client', 'build')));
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html'));
+});
 
 //Server Connection
 app.listen(port, function(){
